6a96834bf9856865307bb57b86cb9e2900d60f46
Andrew Kang <andrewiskang@gmail.com>
2018-04-07 17:58:03 -0700

    added functionality to retry if southwest website takes to different beta URL

--- flight_prices.py
+++ flight_prices.py
@@ -43,46 +43,51 @@ def get_price(flight_info):
     # opens southwest site, searches for specific flight price given params
     # returns output as a RecordedPrice object
 
-    # start up Chrome webdriver and point to Southwest website
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(15)
-    driver.get("https://www.southwest.com")
+    while True:
+        try:
+            # start up Chrome webdriver and point to Southwest website
+            driver = webdriver.Chrome()
+            driver.implicitly_wait(15)
+            driver.get("https://www.southwest.com")
 
-    # toggle one-way, not roundtrip
-    one_way_button = driver.find_element_by_id("trip-type-one-way")
-    one_way_button.click()
+            # toggle one-way, not roundtrip
+            one_way_button = driver.find_element_by_id("trip-type-one-way")
+            one_way_button.click()
 
-    # input origin
-    origin_field = driver.find_element_by_id("air-city-departure")
-    origin_field.clear()
-    origin_field.send_keys(flight_info.origin)
+            # input origin
+            origin_field = driver.find_element_by_id("air-city-departure")
+            origin_field.clear()
+            origin_field.send_keys(flight_info.origin)
 
-    # input destination
-    destination_field = driver.find_element_by_id("air-city-arrival")
-    destination_field.clear()
-    destination_field.send_keys(flight_info.destination)
+            # input destination
+            destination_field = driver.find_element_by_id("air-city-arrival")
+            destination_field.clear()
+            destination_field.send_keys(flight_info.destination)
 
-    # input departure date and submit
-    departure_dt_field = driver.find_element_by_id("air-date-departure")
-    departure_dt_field.clear()
-    departure_dt_field.send_keys(flight_info.departure_dt)
-    departure_dt_field.submit()
+            # input departure date and submit
+            departure_dt_field = driver.find_element_by_id("air-date-departure")
+            departure_dt_field.clear()
+            departure_dt_field.send_keys(flight_info.departure_dt)
+            departure_dt_field.submit()
 
-    # on next page, search for tier elements for the given flight #
-    # there should be 3 tiers: Business Select, Anytime, Wanna Get Away
-    tiers = driver.find_elements_by_xpath("//*[contains(@title,'Departing flight " + str(flight_info.flight_num) + "')]")
+            # on next page, search for tier elements for the given flight #
+            # there should be 3 tiers: Business Select, Anytime, Wanna Get Away
+            tiers = driver.find_elements_by_xpath("//*[contains(@title,'Departing flight " + str(flight_info.flight_num) + "')]")
 
-    # select Wanna Get Away element and extract flight price from value
-    # note from the value, we can extract additional elements like time
-    wga_tier = tiers[-1]
-    wga_elements = wga_tier.get_attribute("value").split("@")
-    wga_price = wga_elements[11]
+            # select Wanna Get Away element and extract flight price from value
+            # note from the value, we can extract additional elements like time
+            wga_tier = tiers[-1]
+            wga_elements = wga_tier.get_attribute("value").split("@")
+            wga_price = wga_elements[11]
 
-    # quit the driver and close all associated windows
-    driver.quit()
+            # quit the driver and close all associated windows
+            driver.quit()
 
-    # return price with timestamp as a RecordedPrice object
-    return RecordedPrice(datetime.datetime.utcnow(), wga_price)
+            # return price with timestamp as a RecordedPrice object
+            return RecordedPrice(datetime.datetime.utcnow(), wga_price)
+        except:
+            print("There was an issue. Retrying...")
+            driver.quit()
 
 def print_current_price(flight_info):
     # prints out flight information as well as its current price
